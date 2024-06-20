678d4587db1b2f0462b6a30a2e7186bd7b3ef0e2
DARSHIK BHUVA <126151773+darshikbhuva@users.noreply.github.com>
2024-06-20 09:56:57 +0530

    Update PriceSection.jsx

--- src/Pages/Dashboard/Componants/PriceSection.jsx
+++ src/Pages/Dashboard/Componants/PriceSection.jsx
@@ -92,7 +92,7 @@ const PriceSection = () => {
             </HStack>
           </TabPanel>
           <TabPanel>
-            <Image src="./Public\graph.svg" width="100%" mt="3rem" />
+            <Image src="./Public/graph.svg" width="100%" mt="3rem" alt="Graph Chart" />
             <HStack justifyContent="space-between">
               {timestamps.map((time) => (
                 <Text color="black.80" key={time} fontSize="sm">
